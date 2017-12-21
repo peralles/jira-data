368200232f85a8e6132bd82607eb71a1f5e0c861
rodrigopecanha86 <34741105+rodrigopecanha86@users.noreply.github.com>
2017-12-21 15:55:22 -0200

    Add files via upload

--- jira-data-management/src/main/java/com/br/peralles/jira/jiradatamanagement/JiraDataManagementApplication.java
+++ jira-data-management/src/main/java/com/br/peralles/jira/jiradatamanagement/JiraDataManagementApplication.java
@@ -1,31 +1,54 @@
 package com.br.peralles.jira.jiradatamanagement;
 
+import com.br.peralles.jira.jiradatamanagement.rest.client.Quote;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import java.nio.charset.Charset;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-import com.br.peralles.jira.jiradatamanagement.rest.client.Quote;
 
 @SpringBootApplication
 public class JiraDataManagementApplication {
-	
+
+	private static final String username = "rodrigo.pecanha@zup.com.br";
+	private static final String password = "488/corrida";
 	private static final Logger log = LoggerFactory.getLogger(JiraDataManagementApplication.class);
 
 	public static void main(String[] args) {
 		//SpringApplication.run(JiraDataManagementApplication.class, args); //not using Spring boot
-		
-	    RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info(quote.toString());
+
+		String url = "https://zupjira.atlassian.net/rest/api/2/project";
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders = createHeadersWithAuthentication();
+
+		HttpEntity<?> requestEntity = new HttpEntity(httpHeaders);
+
+		ResponseEntity<String> projetos = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+
+		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+	}
+
+	static private HttpHeaders createHeadersWithAuthentication() {
+		String plainCreds = username + ":" + password;
+		byte[] base64CredsBytes = Base64.encodeBase64(
+				plainCreds.getBytes(Charset.forName("US-ASCII")) );
+		String base64Creds = new String(base64CredsBytes);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + base64Creds);
+
+		return headers;
 	}
 
 }
-
-
-
-//Example on
-//https://spring.io/guides/gs/consuming-rest/
-
-//About Rest Template
-//http://www.baeldung.com/rest-template
